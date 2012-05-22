f76ca7aca7a0204a324452c6d6c938633f4ad13c
Robert Edmonds <edmonds@isc.org>
2012-05-22 19:50:34 -0400

    dnstable_dump: add -j, -r, -d command line arguments

--- src/dnstable_dump.c
+++ src/dnstable_dump.c
@@ -16,8 +16,8 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <locale.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,18 +27,60 @@
 #include <dnstable.h>
 #include <mtbl.h>
 
+#include "librsf/argv.h"
 #include "librsf/print_string.h"
+
+static bool g_json;
+static bool g_rrset;
+static bool g_rdata;
+static char *g_fname;
+
+static argv_t args[] = {
+	{ 'j',	"json",
+		ARGV_BOOL,
+		&g_json,
+		NULL,
+		"output in JSON format (default: text)" },
+
+	{ 'r',	"rrset",
+		ARGV_BOOL,
+		&g_rrset,
+		NULL,
+		"output rrset records" },
+
+	{ ARGV_ONE_OF },
+
+	{ 'd',	"rdata",
+		ARGV_BOOL,
+		&g_rdata,
+		NULL,
+		"output rdata records" },
+
+	{ ARGV_MAND, NULL,
+		ARGV_CHAR_P,
+		&g_fname,
+		"filename",
+		"input file" },
+
+	{ ARGV_LAST }
+};
 
 static void
 print_entry(struct dnstable_entry *ent)
 {
-	char *s = dnstable_entry_to_text(ent);
-	assert(s != NULL);
-	if (strlen(s) > 0) {
-		fputs(s, stdout);
-		if (dnstable_entry_get_type(ent) == DNSTABLE_ENTRY_TYPE_RRSET)
+	if (dnstable_entry_get_type(ent) == DNSTABLE_ENTRY_TYPE_RRSET ||
+	    dnstable_entry_get_type(ent) == DNSTABLE_ENTRY_TYPE_RDATA)
+	{
+		char *s;
+		if (g_json)
+			s = dnstable_entry_to_json(ent);
+		else
+			s = dnstable_entry_to_text(ent);
+		if (s != NULL) {
+			fputs(s, stdout);
 			putchar('\n');
-		free(s);
+			free(s);
+		}
 	}
 }
 
@@ -54,39 +96,38 @@ do_dump(struct dnstable_iter *it)
 		dnstable_entry_destroy(&ent);
 		count++;
 	}
-	fprintf(stderr, "Dumped %'" PRIu64 " entries.\n", count);
 }
 
 int
 main(int argc, char **argv)
 {
-	setlocale(LC_ALL, "");
-
-	const char *m_fname;
 	struct mtbl_reader *m_reader;
 	struct dnstable_reader *d_reader;
 	struct dnstable_iter *d_it;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <DB FILE>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	m_fname = argv[1];
+	argv_process(args, argc, argv);
 
-	m_reader = mtbl_reader_init(m_fname, NULL);
+	m_reader = mtbl_reader_init(g_fname, NULL);
 	if (m_reader == NULL) {
 		perror("mtbl_reader_init");
 		fprintf(stderr, "dnstable_dump: unable to open database file %s\n",
-			m_fname);
+			g_fname);
 		exit(EXIT_FAILURE);
 	}
 
 	d_reader = dnstable_reader_init(mtbl_reader_source(m_reader));
-	d_it = dnstable_reader_iter(d_reader);
+
+	if (g_rrset)
+		d_it = dnstable_reader_iter_rrset(d_reader);
+	else if (g_rdata)
+		d_it = dnstable_reader_iter_rdata(d_reader);
+
 	do_dump(d_it);
 	dnstable_iter_destroy(&d_it);
 	dnstable_reader_destroy(&d_reader);
 	mtbl_reader_destroy(&m_reader);
+
+	argv_cleanup(args);
 
 	return (EXIT_SUCCESS);
 }
