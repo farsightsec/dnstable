af8c5978ed0ef77f221ded913ed2e0d492a4586e
David Waitzman <djw@fsi.io>
2019-03-28 10:10:09 -0400

    Add dnstable_dupsort_func

--- dnstable/reader.c
+++ dnstable/reader.c
@@ -37,6 +37,12 @@ reader_iter_free(void *);
 static dnstable_res
 reader_iter_next(void *, struct dnstable_entry **);
 
+static int
+dnstable_dupsort_func(void *clos,
+		      const uint8_t *key, size_t len_key,
+		      const uint8_t *val0, size_t len_val0,
+		      const uint8_t *val1, size_t len_val1);
+
 struct dnstable_reader *
 dnstable_reader_init(const struct mtbl_source *source)
 {
@@ -45,6 +51,50 @@ dnstable_reader_init(const struct mtbl_source *source)
 	r->source = source;
 	return (r);
 }
+
+/*
+ * dnstable_dupsort_func is used to sort the entries with duplicate
+ * keys during the merge process based on their data.  The dnstable
+ * data is a triplet of variable-length integers, so (in theory) the
+ * byte comparison used for keys would not suffice.  This is mostly of
+ * interest with a NULL merge function, although it could be used to
+ * enforce some order of merges with a non-NULL merge function.
+ *
+ * This sorts first by time_first, then by time_last if time_first is the same.
+ */
+static int
+dnstable_dupsort_func(void *clos,
+		      const uint8_t *key, size_t len_key,
+		      const uint8_t *val0, size_t len_val0,
+		      const uint8_t *val1, size_t len_val1)
+{
+	uint64_t time_first0, time_last0, count0;
+	uint64_t time_first1, time_last1, count1;
+
+	if (len_key && (key[0] == ENTRY_TYPE_RRSET ||
+			key[0] == ENTRY_TYPE_RDATA))
+	{
+		assert(len_val0 && len_val1);
+		dnstable_res res;
+
+		res = triplet_unpack(val0, len_val0, &time_first0, &time_last0, &count0);
+		assert(res == dnstable_res_success);
+		res = triplet_unpack(val1, len_val1, &time_first1, &time_last1, &count1);
+		assert(res == dnstable_res_success);
+
+		if (time_first0 < time_first1)
+			return -1;
+		if (time_first0 > time_first1)
+			return 1;
+		if (time_last0 < time_last1)
+			return -1;
+		if (time_last0 > time_last1)
+			return 1;
+		return 0;
+	} else
+		return 0;
+}
+
 
 struct dnstable_reader *
 dnstable_reader_init_setfile(const char *setfile)
@@ -59,10 +109,13 @@ dnstable_reader_init_setfile(const char *setfile)
 	/* reset and redo the mtbl fileset options */
 	fopt = mtbl_fileset_options_init();
 	mtbl_fileset_options_set_merge_func(fopt, NULL, NULL);
+	mtbl_fileset_options_set_dupsort_func(fopt, dnstable_dupsort_func, NULL);
 	r->fs_no_merge = mtbl_fileset_dup(r->fs, fopt);
 	mtbl_fileset_options_destroy(&fopt);
+
 	r->source = mtbl_fileset_source(r->fs);
 	r->source_no_merge = mtbl_fileset_source(r->fs_no_merge);
+
 	return (r);
 }
 
@@ -116,9 +169,9 @@ dnstable_reader_iter_rdata_names(struct dnstable_reader *r)
 }
 
 /*
- * dnstable_reader_query returns aggregated
- * (e.g. a dnstable merge function is applied) or unaggregated results
- * depending upon the query object's aggregated flag.
+ * dnstable_reader_query returns aggregated (e.g. a dnstable merge
+ * function is applied) or unaggregated results depending upon the
+ * query object's aggregated flag.
  */
 struct dnstable_iter *
 dnstable_reader_query(struct dnstable_reader *r, struct dnstable_query *q)
