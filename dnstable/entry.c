33f7c21ed2f0764ce53750b3d4883a02f1e376bc
David Waitzman <djw@fsi.io>
2021-06-08 08:31:25 -0400

    Fix small memory leak in dnstable_dump -r -j -R tests/test-dns.mtbl and dnstable_dump -d -j -R tests/test-dns.mtbl

--- dnstable/entry.c
+++ dnstable/entry.c
@@ -392,6 +392,7 @@ dnstable_entry_to_json_fmt(const struct dnstable_entry *e,
 				fmt_hex_str(rbuf, (char*)rrtype, rrtype_str_len);
 				ubuf_cterm(rbuf);
 				ubuf_detach(rbuf, &rbuf_as_str, &rbuf_as_str_len);
+				ubuf_destroy(&rbuf);
 				add_yajl_string(g, (char*)rbuf_as_str);
 				my_free(rbuf_as_str);
 			}
@@ -438,6 +439,7 @@ dnstable_entry_to_json_fmt(const struct dnstable_entry *e,
 			fmt_hex_str(rbuf, (char*)rdata->data, rdata->len);
 			ubuf_cterm(rbuf);
 			ubuf_detach(rbuf, &rbuf_as_str, &rbuf_as_str_len);
+			ubuf_destroy(&rbuf);
 			add_yajl_string(g, (char*)rbuf_as_str);
 			my_free(rbuf_as_str);
 
